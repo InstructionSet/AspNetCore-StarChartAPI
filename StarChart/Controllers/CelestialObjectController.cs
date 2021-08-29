41a874b0a61b2535ed8f82aa9e2443e9eebd0b7e
chris <chris_lewis07@hotmail.com>
2021-08-29 12:53:26 +1000

    Add Routes

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -16,6 +17,48 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var CelestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+
+            if (CelestialObject == null)
+            {
+                return NotFound();
+            }
+
+            CelestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == CelestialObject.Id).ToList();
+
+            return new ObjectResult(CelestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var CelestialObject = _context.CelestialObjects.FirstOrDefault(x => x.Name == name);
+
+            if (CelestialObject == null)
+            {
+                return NotFound();
+            }
+
+            CelestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == CelestialObject.Id).ToList();
+
+            return new ObjectResult(CelestialObject);
+        }
+
+        public IActionResult GetAll()
+        {
+            var CelestialObjects = _context.CelestialObjects.ToList();
+
+            foreach (var CelestialObject in CelestialObjects)
+            {
+                CelestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == CelestialObject.Id).ToList();
+            }
+
+            return new ObjectResult(CelestialObjects);
         }
     }
 }
